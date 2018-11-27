c10f403a7b5ab64e76a57c4252365a4581c9f906
Maria del Mar Vazquez <mariadelmar.vazquez@iesdonana.org>
2018-11-27 21:03:34 +0100

    Carga inicial

---
+++ comunes/auxiliar.php
@@ -0,0 +1,11 @@
+<?php
+function menu() { ?>
+    <ul class="nav nav-pills">
+      <li role="presentation" class="active"><a href="../index.php">Filmaffinity</a></li>
+      <li role="presentation"><a href="../peliculas/index.php">Peliculas</a></li>
+      <li role="presentation"><a href="../generos/index.php">Generos</a></li>
+    </ul>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<?php }
+?>
