0d46ff83ffa6e526170552af60d9467755d7cf22
vmmarr <mariadelmar.vazquez@iesdonana.org>
2018-11-27 23:58:30 +0100

    Metodo cabecera y algo de insertar

--- peliculas/index.php
+++ peliculas/index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Peliculas</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <?php
+        require '../comunes/auxiliar.php';
+        cabecera('Peliculas');
+        ?>
     </head>
     <body>
         <?php
-        require '../comunes/auxiliar.php';
         menu();
         ?>
     </body>
